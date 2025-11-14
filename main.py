19acfb9e58e91332aaf58baa080b27a3bab42922
Nathan Svensson <nathan.svensson@icloud.com>
2025-11-14 11:34:39 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Time
+days_worked = int(input("How many days did you work this week: "))
+hours_per_day = float(input("How many hours per day: "))
+
+# Rate
+hourly_rate = float(input("What is your hourly rate: £"))
+
+# Overtime
+did_overtime = bool(input("Did you work overtime (y/n): "))
+overtime_rate = float(input("What is the overtime rate (1.5 etc): "))
